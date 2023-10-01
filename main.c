ec0b6c4fee02a319f9bc9bfc3594a92007fda512
Adel Suarez <adel90suarez@gmail.com>
2023-09-30 20:47:59 -0400

    menu creation and code testing

--- main.c
+++ main.c
@@ -1,5 +1,43 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-void main(){
-    printf("Hola mundo");
+// Variables
+char option;
+
+// Function
+void optUno()
+{
+    printf("Opcion Uno\n");
+}
+
+void optDos()
+{
+    printf("Opcion dos\n");
+}
+
+// Main
+int main()
+{
+    while (true)
+    {
+        printf("Introduce la opcion: ");
+        scanf("%s", &option);
+        if (option == '1')
+        {
+            optUno();
+        }
+        else if (option == '2')
+        {
+            optDos();
+        }
+        else if (option == '0')
+        {
+            break;
+        }
+        else
+        {
+            printf("La opcion no existe\n");
+        }
+    }
+    return 0;
 }
